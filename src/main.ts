633e7893acf4f1759c2236a1547f8a08134f7e2a
chenmh <chenmh@sucsoft.com>
2021-12-31 17:00:20 +0800

    refactor: 引入了ElLoading

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import router from './router'
 import store from './store'
 import App from './App.vue'
 
+import {ElLoading} from "element-plus"  // 不用use引入会报错v-loading
+
 createApp(App)
     .use(store)
+    .use(ElLoading)
     .use(router)
     .mount('#app')
