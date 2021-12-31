f31c8682fb3fc6637fb6e13ecdd0c33530def492
ChenMH <2036604698@qq.com>
2021-12-31 23:22:04 +0800

    feat: vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,19 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    modules: {
+        // region [module:diskModule] 获取磁盘信息相关
+        diskModule: {
+            namespaced: true,
+            state: {
+                diskNameList: [] as string[]
+            },
+            mutations: {
+                updateNameList(state: any, newList: string[]) {
+                    state.diskNameList = newList
+                }
+            }
+        }
+        // endregion
+    }
 })
