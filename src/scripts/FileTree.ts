80f56df65c96170f92227285ece1c5e211070efd
chenmh <chenmh@sucsoft.com>
2021-12-31 17:57:02 +0800

    style: 提取单独的接口类型

--- src/scripts/FileTree.ts
+++ src/scripts/FileTree.ts
@@ -7,20 +7,8 @@ namespace FileTree {
     const myLog = require("../scripts/Logger").myLog;
 
     // region 接口
-    /**
-     * @description 解析后的文件树
-     */
-    interface FileTreeNode {
-        uuid: string  // 节点的id - 可用作表格的row-key
-        type: 'DIR' | 'FILE'  // 目录/文件
-        root: string  // 根目录/磁盘名
-        base: string  // 文件名+扩展名
-        name: string  // 文件名
-        ext: string  // 扩展名
-        dir: string  // 绝对路径
-        ifDeep: boolean  // 是否获取了子目录信息
-        children?: FileTreeNode[]  // 子目录
-    }
+    // @ts-ignore TODO
+    import {FileTreeNode} from "@/scripts/interface";
     // endregion
 
     // region 类
@@ -169,7 +157,7 @@ namespace FileTree {
     // endregion
 }
 
-// const TreeClass = FileTree.FileTree
+// export type FileTreeNode = FileTree.FileTreeNode
 module.exports = {
     TreeClass: FileTree.FileTree
 }
