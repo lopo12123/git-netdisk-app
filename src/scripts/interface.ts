ad8b6623515a7c1e7ba552bab746b0ef0e745abe
ChenMH <2036604698@qq.com>
2022-01-02 23:36:32 +0800

    doc: 添加正确的注释

--- src/scripts/interface.ts
+++ src/scripts/interface.ts
@@ -11,15 +11,24 @@ interface DiskInfo {
  * @description 解析后的文件树
  */
 interface FileTreeNode {
-    uuid: string  // 节点的id - 可用作表格的row-key
-    type: 'DIR' | 'FILE'  // 目录/文件
-    root: string  // 根目录/磁盘名
-    base: string  // 文件名+扩展名
-    name: string  // 文件名
-    ext: string  // 扩展名
-    dir: string  // 绝对路径
-    ifDeep: boolean  // 是否获取了子目录信息
-    children?: FileTreeNode[]  // 子目录
+    // 节点的id - 可用作表格的row-key
+    uuid: string
+    // 目录/文件
+    type: 'DIR' | 'FILE'
+    // 根目录/磁盘名
+    root: string
+    // 文件名+扩展名
+    base: string
+    // 文件名
+    name: string
+    // 扩展名
+    ext: string
+    // 绝对路径
+    dir: string
+    // 是否获取了子目录信息
+    ifDeep: boolean
+    // 子目录
+    children?: FileTreeNode[]
 }
 
 export {
