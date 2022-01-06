6c9943ee3d0729020c56aec80d8b87696855eada
ChenMH <2036604698@qq.com>
2022-01-06 23:53:55 +0800

    feat: 构建、扩展文件树优化

--- src/views/Tree.vue
+++ src/views/Tree.vue
@@ -10,9 +10,15 @@
         <el-table
             v-if="ifTree" class="tree-table"
             :data="treeData" row-key="uuid"
+            @row-contextmenu="contextMenu"
             height="100%" stripe>
 
-            <el-table-column label="类型" prop="type"/>
+            <el-table-column label="类型">
+                <template #default="scope">
+                    <folder-open-outlined v-if="scope.row.type === 'DIR'"/>
+                    <file-text-outlined v-if="scope.row.type === 'FILE'"/>
+                </template>
+            </el-table-column>
 
             <el-table-column label="全名" prop="base"/>
 
@@ -23,33 +29,43 @@
             <el-table-column label="路径" prop="dir"/>
 
         </el-table>
+        <context-menu :style="`left: ${left}px; top: ${top}px`"
+                      @item-click="itemClick"/>
     </div>
 </template>
 
 <script lang="ts">
-import {defineComponent, Ref, ref} from "vue";
+import {defineComponent, onBeforeUnmount, Ref, ref} from "vue";
+import {useRouter} from "vue-router";
+import {v4 as uuid} from "uuid";
 import {FileTreeNode} from "@/scripts/interface";
 import {ElMessage, ElTable, ElTableColumn} from "element-plus";
-import router from "@/router";
+import {FolderOpenOutlined, FileTextOutlined} from "@ant-design/icons-vue";
+import ContextMenu from "@/components/Tree/ContextMenu.vue"
+import {sendIpcExplorer} from "@/scripts/Ipc";
 
 export default defineComponent({
     name: "Tree",
     components: {
-        ElTable, ElTableColumn
+        ElTable, ElTableColumn,
+        FolderOpenOutlined, FileTextOutlined,
+        ContextMenu
     },
     setup() {
+        const router = useRouter()
         const ifTree = ref(false)
-        const treeData: Ref<FileTreeNode[]> = ref([{}])
+        const treeData: Ref<FileTreeNode[]> = ref([])
         const treeJsonStr = sessionStorage.getItem('tree')
         try {
             if(treeJsonStr === null) throw new Error('NULL')
             treeData.value = [JSON.parse(treeJsonStr)]
             ifTree.value = true
-            ElMessage({
-                type: 'success',
-                message: 'parse file tree successfully'
-            })
-        } catch (e) {
+            // ElMessage({
+            //     type: 'success',
+            //     message: 'parse file tree successfully'
+            // })
+        }
+        catch (e) {
             ifTree.value = false
             ElMessage({
                 type: 'warning',
@@ -60,8 +76,44 @@ export default defineComponent({
             router.push({name: 'Home'})
         }
 
+        let rowClicked: FileTreeNode | null = null  // 存储当前row用于展开
+        const [left, top] = [ref(-500), ref(-500)]  // 右键菜单的位置
+        const contextMenu = (row: any, column: any, e: PointerEvent) => {
+            const { clientWidth, clientHeight } = document.body
+            const { clientX, clientY } = e
+            left.value = clientX < (clientWidth - 130) ? (clientX + 5) : (clientX - 130)
+            top.value = clientY < (clientHeight - 80) ? (clientY - 50) : (clientY - 130)
+            rowClicked = row
+        }
+        const hideContextMenu = () => {
+            left.value = -500
+            top.value = -500
+        }
+        const itemClick = (type: 'Expansion' | 'Explorer') => {
+            if(type === 'Expansion') {
+                // ipc expand
+            }
+            else if(type === 'Explorer') {
+                if(rowClicked === null) return
+                else if(rowClicked.type === 'DIR') {
+                    sendIpcExplorer({uuid: uuid(), dir: rowClicked.dir+rowClicked.base})
+                }
+                else {
+                    sendIpcExplorer({uuid: uuid(), dir: rowClicked.dir})
+                }
+            }
+            console.log(type, '了')
+        }
+
+
+        document.addEventListener('click', hideContextMenu)
+        onBeforeUnmount(() => {
+            document.removeEventListener('click', hideContextMenu)
+        })
+
         return {
-            ifTree, treeData, backToHome
+            ifTree, treeData, backToHome,
+            left, top, contextMenu, itemClick,
         }
     }
 })
